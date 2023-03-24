2d2442c8e07c3565781ff32f25f2dd87d61224af
bell-ho <jj0101065@gmail.com>
2023-03-24 12:11:17 +0900

    back - 포트폴리오 목록 출력

--- back/src/main/java/com/portfolio/back/dto/PortfolioRes.java
+++ back/src/main/java/com/portfolio/back/dto/PortfolioRes.java
@@ -1,2 +1,29 @@
-package com.portfolio.back.dto;public class PortfolioRes {
+package com.portfolio.back.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.portfolio.back.domain.Portfolio;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class PortfolioRes {
+
+    private Long id;
+    private String name;
+    @JsonFormat(pattern = "yy.MM.dd")
+    private LocalDateTime createDate;
+    @JsonFormat(pattern = "yy.MM.dd")
+    private LocalDateTime modifiedDate;
+
+    @Builder
+    public PortfolioRes(Portfolio portfolio) {
+        this.id = portfolio.getId();
+        this.name = portfolio.getName();
+        this.createDate = portfolio.getCreatedDate();
+        this.modifiedDate = portfolio.getModifiedDate();
+    }
 }
