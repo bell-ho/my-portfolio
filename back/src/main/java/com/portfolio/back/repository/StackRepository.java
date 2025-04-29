b656e3614d58983a4c3f6cfd7d8fdcdf2e620d5e
bell-ho <62537935+bell-ho@users.noreply.github.com>
2025-04-29 14:10:03 +0900

    Update StackRepository.java

--- back/src/main/java/com/portfolio/back/repository/StackRepository.java
+++ back/src/main/java/com/portfolio/back/repository/StackRepository.java
@@ -1,5 +1,6 @@
 package com.portfolio.back.repository;
-
+import javax.persistence.LockModeType;
+import org.springframework.data.jpa.repository.Lock;
 import com.portfolio.back.domain.Stack;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
@@ -9,5 +10,7 @@ import java.util.Optional;
 
 @Repository
 public interface StackRepository extends JpaRepository<Stack, Long>, QuerydslPredicateExecutor<Stack> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Stack> findByName(String name);
 }
